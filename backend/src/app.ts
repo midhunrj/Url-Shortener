c3f4719685fc28b39781a241023b4b4ca82f6021
midhunrj <midhunrj18852@gmail.com>
2025-03-22 16:20:54 +0530

    change in cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,13 +13,19 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-const corsOptions:CorsOptions={
-    origin:[process.env.CLIENT_URL!,"http://localhost:5173"],
-    credentials:true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-}
-app.options("*",cors(corsOptions))
+// const corsOptions:CorsOptions={
+//     origin:[process.env.CLIENT_URL!,"http://localhost:5173"],
+//     credentials:true,
+//     allowedHeaders: ["Content-Type", "Authorization"],
+// }
+const corsOptions = {
+    origin: [process.env.CLIENT_URL!, "http://localhost:5173", "https://url-shortener-rho-amber.vercel.app"], // Add frontend URLs
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Ensure all required methods are allowed
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow required headers
+};
 app.use(cors(corsOptions)) 
+app.options("*",cors(corsOptions))
 
 mongoose.connect(process.env.MONGODB_URL!)
 .then(()=>{
@@ -33,6 +39,14 @@ mongoose.connect(process.env.MONGODB_URL!)
 
 console.log("AccessTokenSecret:", process.env.ACCESS_TOKEN_SECRET);
 console.log("RefreshTokenSecret:", process.env.REFRESH_TOKEN_SECRET);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL!);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.use('/',userRouter)
 app.listen(7986,()=>{
